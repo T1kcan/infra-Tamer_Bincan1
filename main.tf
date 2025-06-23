00cb5336b81e0a767428a47cbada487035aa99cc
T1kcan <97545634+T1kcan@users.noreply.github.com>
2025-06-24 01:38:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "proxmox_vm_qemu" "master" {
   ciuser      = "cluster"
   cipassword  = "cluster"
   ipconfig0   = "ip=192.168.0.10${count.index + 1}/24,gw=192.168.0.1"
-  sshkeys     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDuqViGM+Y6E8uOFNWMaI3e7Qhr/HBTJzZQOhoAWb88AjJpdnatyEXnsCUKcuLBilu01fqLYG3jlU3Zj8x9T86wqCSrQm2t3eq25/TB9IJLGfByBlGrx60LXRGJQRvBlyZbkEhdv9AKmkSZ4WW4u+0q2Nc3HyQgtov3aPelzikPAjBbvCCAGRwC6d4XWZYgvezn2OJ15VET+rf+toZhJsmkUzrIuNLSmfekHgxTQlcFycCF9ifM+UxFPU/FOFeiinON/mt0C5EHVnzsF+bSu2rA5boz2Z1ooQA18pH6pEHgKHOcw88ozY2TCYnwmh+wtzixgxnG5C9SDQUOqMH6fWNtMWdTmYw2uzuAfgyUhqEIkHbXwFMj8wLs96FtGfdwBPsT6c5hvPZpenWVFBZbThP1wd9170j9Ey1oASGeeNevZGbAYDKFzp4rwQVUHHgN8IzKHQrh75kfVNiO+B2KocDmdblOtf0f7kmM8B0EJHBO+nnxd3cBivHHgs1TIu1Tt1g+b2bv23zUsNqnSqf1vnQynuUHSVKjYkLsSowSEtESP7S7d0SReYCs4UhMyNKTEB5Zv7EOBQ1GqIczhJrSXeNtholuF6PeyYwKQfaaTM6t45Yl4jYUxJIRr/LQbACSo/QKmC5qbvCqIGG4a+HnqJslft2sRMFI77E7te5w2IMKKQ== tbincan@gmail.com"
+  sshkeys     = "ssh-rsa AAAAB.."
   cores       = 2
   memory      = 2048
   sockets     = 1
@@ -74,7 +74,7 @@ resource "proxmox_vm_qemu" "worker" {
   ciuser      = "cluster"
   cipassword  = "cluster"
   ipconfig0   = "ip=192.168.0.11${count.index + 1}/24,gw=192.168.0.1"
-  sshkeys     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDuqViGM+Y6E8uOFNWMaI3e7Qhr/HBTJzZQOhoAWb88AjJpdnatyEXnsCUKcuLBilu01fqLYG3jlU3Zj8x9T86wqCSrQm2t3eq25/TB9IJLGfByBlGrx60LXRGJQRvBlyZbkEhdv9AKmkSZ4WW4u+0q2Nc3HyQgtov3aPelzikPAjBbvCCAGRwC6d4XWZYgvezn2OJ15VET+rf+toZhJsmkUzrIuNLSmfekHgxTQlcFycCF9ifM+UxFPU/FOFeiinON/mt0C5EHVnzsF+bSu2rA5boz2Z1ooQA18pH6pEHgKHOcw88ozY2TCYnwmh+wtzixgxnG5C9SDQUOqMH6fWNtMWdTmYw2uzuAfgyUhqEIkHbXwFMj8wLs96FtGfdwBPsT6c5hvPZpenWVFBZbThP1wd9170j9Ey1oASGeeNevZGbAYDKFzp4rwQVUHHgN8IzKHQrh75kfVNiO+B2KocDmdblOtf0f7kmM8B0EJHBO+nnxd3cBivHHgs1TIu1Tt1g+b2bv23zUsNqnSqf1vnQynuUHSVKjYkLsSowSEtESP7S7d0SReYCs4UhMyNKTEB5Zv7EOBQ1GqIczhJrSXeNtholuF6PeyYwKQfaaTM6t45Yl4jYUxJIRr/LQbACSo/QKmC5qbvCqIGG4a+HnqJslft2sRMFI77E7te5w2IMKKQ== tbincan@gmail.com"
+  sshkeys     = "ssh-rsa AAAAB.."
   cores       = 2
   memory      = 2048
   sockets     = 1
